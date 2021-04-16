a908e9152d085253f029c5c92a0d2edd500a3c66
changkai <18562122962@163.com>
2021-04-16 09:42:14 +0800

    新建项目

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
 </template>
 
 <script lang="ts">
+// defineComponent函数，目的是定义一个组件，内部可以传入一个配置对象
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
+// 暴露出去一个定义好的组件
 export default defineComponent({
   name: 'App',
   components: {
