cc037fc3ac01ad6e6159154adb85a09ccbeb79f1
changkai <18562122962@163.com>
2021-04-16 10:42:40 +0800

    setup和ref使用

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h2>setup和reactive的基本使用</h2>
+   
 </template>
 
 <script lang="ts">
 // defineComponent函数，目的是定义一个组件，内部可以传入一个配置对象
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 // 暴露出去一个定义好的组件
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 笔记/01_setup和ref的基本使用/01_ref使用.vue
@@ -0,0 +1,29 @@
+<template>
+    <h2>setup和ref的基本使用</h2>
+    <h3>{{ number }}</h3>
+    <button @click="increase">按钮</button>
+</template>
+
+<script lang="ts">
+// defineComponent函数，目的是定义一个组件，内部可以传入一个配置对象
+import { defineComponent, ref } from "vue";
+
+// 暴露出去一个定义好的组件
+export default defineComponent({
+    name: "App",
+    setup() {
+        // ref定义一个响应式的数据。返回一个ref对象，对象中有一个value属性，如果对数据进行操作，需要使用.value的方式进行修改
+        // html模板不需要.value属性的写法
+        // ref一般用来定义一个基本类型的响应式数据
+        let number = ref(0)
+        // console.log(number) // 是一个对象
+        function increase() {
+            number.value++
+        }
+        return {
+            number,
+            increase
+        }
+    }
+});
+</script>
