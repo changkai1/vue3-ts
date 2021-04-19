c5bc9408c4414c95c4c58fd8aebabb9788cf7c54
changkai <18562122962@163.com>
2021-04-19 17:46:47 +0800

    hook使用及axios封装

---
+++ 笔记/06_hook使用/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>自定义hook函数：自定义hook的作用类似于vue2中的mixin技术</h2>
+        <hr />
+        <h4>x: {{ x }} | y：{{ y }}</h4>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import useMousePosition from './hooks/useMousePosition';
+export default defineComponent({
+    name: 'App',
+    setup() {
+        const { x, y } = useMousePosition();
+        return {
+            x,
+            y
+        }
+    }
+})
+</script>
+<style scoped>
+    
+</style>
