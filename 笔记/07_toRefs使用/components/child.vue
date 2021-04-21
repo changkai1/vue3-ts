87d08d9a3bf49e98890aa84ed3909427739d57e7
changkai <18562122962@163.com>
2021-04-21 09:28:21 +0800

    toRefs使用

---
+++ 笔记/07_toRefs使用/components/child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>child子组件</h2>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Child',
+    props: {
+        foo: {
+            type: String
+        },
+        bar: {
+            type: String
+        }
+    },
+    setup(props) {
+        console.log(props)
+        return {
+
+        }
+    }
+})
+</script>
+<style scoped>
+    
+</style>
