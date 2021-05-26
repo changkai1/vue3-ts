6a2064e31fb5f27dc1dfec14964a24888d0bca1d
changkai <18562122962@163.com>
2021-05-26 18:04:58 +0800

    mustache语法

---
+++ 02_Vue3基本指令/vue2.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    
+    </div>
+</template>
+<script>
+export default {
+    name: '',
+    data() {
+
+    },
+    methods: {
+        
+    },
+    created () {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
