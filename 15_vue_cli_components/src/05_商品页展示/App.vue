464cc4d6e54e198ece4c5f6e5450c14eb8f7a82c
changkai <18562122962@163.com>
2021-07-08 10:23:32 +0800

    商品展示页练习

---
+++ 15_vue_cli_components/src/05_商品页展示/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app">
+        <tab-control
+            :titles="titles"
+            @itemIndex="itemIndex"></tab-control>
+        <div>
+            {{ content[currentIndex] }}
+        </div>
+    </div>
+</template>
+<script>
+import tabControl from "./tabControl.vue"
+export default {
+    name: '',
+    components: {
+        tabControl
+    },
+    data() {
+        return {
+            titles: ["衬衣", "鞋子", "裤子"],
+            content: ["衬衣页面", "鞋子页面", "裤子页面"],
+            currentIndex: 0
+        }
+    },
+    methods: {
+        itemIndex(index) {
+            this.currentIndex = index
+        }
+    },
+    created () {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
---
+++ 15_vue_cli_components/src/05_商品页展示/tabControl.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="tab-control">
+        <div class="tab-control-item"
+            v-for="(item, index) in titles"
+            :key="item"
+            :class="{active: currentIndex === index}"
+            @click="itemClick(index)">
+            <span>{{ item }}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    emits: ["itemIndex"],
+    props: {
+        titles: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    },
+    name: '',
+    data() {
+        return {
+            currentIndex: 0
+        }
+    },
+    methods: {
+        itemClick(index) {
+            this.currentIndex = index
+            this.$emit("itemIndex", index)
+        }
+    },
+    created () {
+        
+    }
+}
+</script>
+<style scoped>
+.tab-control {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.tab-control-item {
+    flex: 1;
+    text-align: center;
+}
+.active {
+    color: red;
+    
+}
+.active span {
+    border-bottom: 3px solid red;
+}
+.tab-control-item span {
+    padding: 5px 10px;
+}
+</style>
