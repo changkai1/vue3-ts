48bcbc424c6fedb606609d444053f3b464c3f431
changkai <18562122962@163.com>
2021-07-15 09:04:47 +0800

    自定义组件的v-model

---
+++ 15_vue_cli_components/src/15_组件的v-model/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>App组件</h2>
+        <h3>{{ message }}</h3>
+        <h4>{{ title }}</h4>
+        <!-- 
+            组件使用一个v-model,默认执行
+            <form-input :modelValue="message" @update:model-value="message = $event"></form-input>
+         -->
+        <!-- <from-input v-model="message"></from-input> -->
+
+        <!-- 组件使用多个v-model -->
+        <from-input v-model="message" v-model:title="title"></from-input>
+    </div>
+</template>
+<script>
+import FromInput from "./FromInput.vue"
+export default {
+    name: '',
+    components: {
+        FromInput
+    },
+    data() {
+        return {
+            message: "Hellow App",
+            title: "Hellow Title"
+        };
+    },
+    methods: {
+        
+    },
+    created () {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
---
+++ 15_vue_cli_components/src/15_组件的v-model/FromInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h2>FromInput组件</h2>
+        <input type="text" v-model="inputValue">
+
+        <input type="text" v-model="titleValue">
+    </div>
+</template>
+<script>
+export default {
+    name: '',
+    props: {
+        // v-model默认传值
+        modelValue: String,
+        title: String
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+        inputValue: {
+            get() {
+                return this.modelValue
+            },
+            set(val) {
+                this.$emit("update:modelValue", val)
+            }
+        },
+        titleValue: {
+            get() {
+                return this.title
+            },
+            set(val) {
+                this.$emit("update:title", val)
+            }
+        }
+    },
+    methods: {
+        
+    },
+    created () {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
